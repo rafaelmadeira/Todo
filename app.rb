e1d9323d8279e33913474271942586b247827890
Tim Gleeson <tim.j.gleeson@gmail.com>
2011-08-24 22:47:05 +1000

    Major update. Added lists for items.

--- app.rb
+++ app.rb
@@ -9,21 +9,43 @@ class Item
   property :id, Serial
   property :content, Text, :required => true
   property :completed, Boolean, :required => true, :default => false
+  property :list, Integer, :required => true
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
+class List 
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, Text, :required => true
   property :created_at, DateTime
   property :updated_at, DateTime
 end
 
 Item.auto_migrate! unless Item.storage_exists?
+List.auto_migrate! unless List.storage_exists?
 
 get '/' do
-  @items = Item.all :order => :id.desc 
-  erb :list
+  @lists = List.all :order => :id.asc
+  @items = Item.all :order => :id.asc
+  erb :index
 end
 
 #add an item to your list
-post '/' do
+post '/item' do
   n = Item.new  
-  n.content = params[:content]  
+  n.content = params[:content] 
+  n.list = params[:list] 
+  n.created_at = Time.now  
+  n.updated_at = Time.now  
+  n.save
+  redirect '/'
+end
+
+#add an item to your list
+post '/list' do
+  n = List.new  
+  n.title = params[:title]  
   n.created_at = Time.now  
   n.updated_at = Time.now  
   n.save
@@ -31,7 +53,7 @@ post '/' do
 end
 
 #update the item in your list
-put '/:id' do  
+put '/item/:id' do  
   i = Item.get params[:id]  
   i.completed = i.completed ? 0 : 1
   i.updated_at = Time.now  
@@ -41,8 +63,14 @@ end
 
 #remove the item in your list
 
-delete '/:id' do  
+delete '/item/:id' do  
   i = Item.get params[:id]  
+  i.destroy  
+  redirect '/'
+end  
+
+delete '/list/:id' do  
+  i = List.get params[:id]  
   i.destroy  
   redirect '/'
 end  
