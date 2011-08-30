2c2825d47690e8a0e4989303ead74992a08c3ac8
Tim Gleeson <tim.j.gleeson@gmail.com>
2011-08-30 14:57:00 +1000

    Update to use user sessions and give people separate lists

--- app.rb
+++ app.rb
@@ -4,12 +4,16 @@ require 'datamapper'
 
 DataMapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/app.db")
 
+enable :sessions
+use Rack::Session::Cookie
+
 class Item
   include DataMapper::Resource
   property :id, Serial
   property :content, Text, :required => true
   property :completed, Boolean, :required => true, :default => false
   property :list, Integer, :required => true
+  property :owner, Text, :required => true
   property :created_at, DateTime
   property :updated_at, DateTime
 end
@@ -18,17 +22,49 @@ class List
   include DataMapper::Resource
   property :id, Serial
   property :title, Text, :required => true
+  property :owner, Text, :required => true
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
+class Owner
+  include DataMapper::Resource
+  property :id, Serial
+  property :cookie, Text, :required => true
+  property :first_name, Text
+  property :last_name, Text
+  property :email, Text
   property :created_at, DateTime
   property :updated_at, DateTime
 end
 
 Item.auto_migrate! unless Item.storage_exists?
 List.auto_migrate! unless List.storage_exists?
+Owner.auto_migrate! unless Owner.storage_exists?
+
+def generate_random_string(length=6)
+  chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ23456789'
+  string = ''
+  length.times { |i| string << chars[rand(chars.length)] }
+  string
+end
 
 get '/' do
-  @lists = List.all :order => :id.asc
-  @items = Item.all :order => :id.asc
+  #set owner ID
+  @owner = request.cookies["owner"]
+  if !@owner
+    @owner = generate_random_string(12)
+    response.set_cookie('owner', :value => @owner, :expires => Time.now + 360000000, :path => '/')
+  end
+  @lists = List.all(:owner => @owner, :order => :id.asc)
+  @items = Item.all(:owner => @owner, :order => :id.asc)
   erb :index
+end
+
+get '/owner/:owner' do
+  response.set_cookie('owner', :value => nil, :expires => Time.now - 1000, :path => '/')
+  response.set_cookie('owner', :value => params[:owner], :expires => Time.now + 360000000, :path => '/')
+  redirect '/'
 end
 
 #add an item to your list
@@ -36,16 +72,18 @@ post '/item' do
   n = Item.new  
   n.content = params[:content] 
   n.list = params[:list] 
+  n.owner = request.cookies["owner"]
   n.created_at = Time.now  
   n.updated_at = Time.now  
   n.save
   redirect '/'
 end
 
-#add an item to your list
+#add a list
 post '/list' do
   n = List.new  
   n.title = params[:title]  
+  n.owner = request.cookies["owner"]
   n.created_at = Time.now  
   n.updated_at = Time.now  
   n.save
@@ -62,7 +100,6 @@ put '/item/:id' do
 end  
 
 #remove the item in your list
-
 delete '/item/:id' do  
   i = Item.get params[:id]  
   i.destroy  
